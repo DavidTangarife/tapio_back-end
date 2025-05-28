4778fd97f47aecf0dcc1aaeca9efd4ee495a107a
DSTV <9871@holbertonstudents.com>
2025-05-28 14:47:40 +1000

    Tapio backend container image & dockercompose

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import cors from 'cors';
-import emailRoutes from "./routes/email.routes";
+import cors from "cors";
+import projectRoutes from "./routes/project.routes";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api", emailRoutes);
+app.use("/api", projectRoutes);
 
 export default app;

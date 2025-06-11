c7965d7835008464fcd2c0e425aa905253ad0403
DSTV <9871@holbertonstudents.com>
2025-06-11 11:51:28 +1000

    fix opportunity controller

--- src/controllers/opportunity.controller.ts
+++ src/controllers/opportunity.controller.ts
@@ -1,11 +1,13 @@
-import { Request, Response } from 'express';
-import { createOpportunity } from '../services/opportunity.services';
-import { ObjectId } from "bson"
+import { Request, Response } from "express";
+import { createOpportunity } from "../services/opportunity.services";
+import { ObjectId } from "bson";
 
-
-export const createOpportunityController = async (req: Request, res: Response) => {
+export const createOpportunityController = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
   const { projectId, statusId, title, company } = req.body;
-   if (!projectId || !statusId || !title || !company?.name) {
+  if (!projectId || !statusId || !title || !company?.name) {
     return res.status(400).json({ error: "Missing required fields." });
   }
 
@@ -14,12 +16,11 @@ export const createOpportunityController = async (req: Request, res: Response) =
       projectId: new ObjectId(String(projectId)),
       statusId: new ObjectId(String(statusId)),
       title,
-      company
+      company,
     });
 
     res.status(201).json(project);
-  } catch (err:any) {
+  } catch (err: any) {
     res.status(500).json({ error: err.message });
   }
 };
-
